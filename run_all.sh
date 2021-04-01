7b178bd49ca12d84a887bb19da9fa31355b49a7c
Anton Klenitskiy <antklen@users.noreply.github.com>
2021-04-01 18:01:42 +0300

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,10 @@
-# python src/prepare_data.py
+python src/prepare_data.py
 
-# python src/train_tokenizers.py
+python src/train_tokenizers.py
 
-# python src/train_lm.py --config_path=src/configs/train_lm1.yaml
-# python src/train_lm.py --config_path=src/configs/train_lm2.yaml
-# python src/train_lm.py --config_path=src/configs/train_lm3.yaml
+python src/train_lm.py --config_path=src/configs/train_lm1.yaml
+python src/train_lm.py --config_path=src/configs/train_lm2.yaml
+python src/train_lm.py --config_path=src/configs/train_lm3.yaml
 
 python src/train.py --config_path=src/configs/train1.yaml
 python src/train.py --config_path=src/configs/train2.yaml
